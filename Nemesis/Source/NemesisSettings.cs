d91a2ffa5ed9bf0d82d23154fd9665badfb3bfa1
AzraelGodKing <AzraelGodsKing@gmail.com>
2026-07-20 03:54:10 -0400

    Nemesis Mod

---
+++ Nemesis/Source/NemesisSettings.cs
@@ -0,0 +1,73 @@
+using Verse;
+
+namespace Nemesis
+{
+    public class NemesisSettings : ModSettings
+    {
+        public float killedAllyChance = 0.15f;
+        public float prisonerEscapedChance = 0.10f;
+        public float slaveEscapedChance = 0.12f;
+        public float fixationChance = 0.10f;
+        public float woundedEscapeChance = 0.12f;
+        public int truceDurationDays = 30;
+        public int maxEscapes = 4;
+
+        public float maxAggressionCap = 0.6f;
+        public float escalationRatePerDay = 0.06f;
+        public int minActionCooldownTicks = 90000;
+
+        public float actionWeightTaunt = 0.35f;
+        public float actionWeightRaid = 0.15f;
+        public float actionWeightAssault = 0.15f;
+        public float actionWeightWaste = 0.08f;
+        public float actionWeightFakeSignal = 0.10f;
+        public float actionWeightCaravan = 0.07f;
+        public float actionWeightSabotage = 0.05f;
+        public float actionWeightFood = 0.05f;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref killedAllyChance, "killedAllyChance", 0.15f);
+            Scribe_Values.Look(ref prisonerEscapedChance, "prisonerEscapedChance", 0.10f);
+            Scribe_Values.Look(ref slaveEscapedChance, "slaveEscapedChance", 0.12f);
+            Scribe_Values.Look(ref fixationChance, "fixationChance", 0.10f);
+            Scribe_Values.Look(ref woundedEscapeChance, "woundedEscapeChance", 0.12f);
+            Scribe_Values.Look(ref truceDurationDays, "truceDurationDays", 30);
+            Scribe_Values.Look(ref maxEscapes, "maxEscapes", 4);
+            Scribe_Values.Look(ref maxAggressionCap, "maxAggressionCap", 0.6f);
+            Scribe_Values.Look(ref escalationRatePerDay, "escalationRatePerDay", 0.06f);
+            Scribe_Values.Look(ref minActionCooldownTicks, "minActionCooldownTicks", 90000);
+            Scribe_Values.Look(ref actionWeightTaunt, "actionWeightTaunt", 0.35f);
+            Scribe_Values.Look(ref actionWeightRaid, "actionWeightRaid", 0.15f);
+            Scribe_Values.Look(ref actionWeightAssault, "actionWeightAssault", 0.15f);
+            Scribe_Values.Look(ref actionWeightWaste, "actionWeightWaste", 0.08f);
+            Scribe_Values.Look(ref actionWeightFakeSignal, "actionWeightFakeSignal", 0.10f);
+            Scribe_Values.Look(ref actionWeightCaravan, "actionWeightCaravan", 0.07f);
+            Scribe_Values.Look(ref actionWeightSabotage, "actionWeightSabotage", 0.05f);
+            Scribe_Values.Look(ref actionWeightFood, "actionWeightFood", 0.05f);
+        }
+
+        public void ResetToDefaults()
+        {
+            killedAllyChance = 0.15f;
+            prisonerEscapedChance = 0.10f;
+            slaveEscapedChance = 0.12f;
+            fixationChance = 0.10f;
+            woundedEscapeChance = 0.12f;
+            truceDurationDays = 30;
+            maxEscapes = 4;
+            maxAggressionCap = 0.6f;
+            escalationRatePerDay = 0.06f;
+            minActionCooldownTicks = 90000;
+            actionWeightTaunt = 0.35f;
+            actionWeightRaid = 0.15f;
+            actionWeightAssault = 0.15f;
+            actionWeightWaste = 0.08f;
+            actionWeightFakeSignal = 0.10f;
+            actionWeightCaravan = 0.07f;
+            actionWeightSabotage = 0.05f;
+            actionWeightFood = 0.05f;
+        }
+    }
+}
